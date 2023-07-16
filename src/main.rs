4305b3ef5539358f8d230e2384a1fa41da6e9af7
0xabstracted <104343984+0xabstracted@users.noreply.github.com>
2023-07-17 01:00:21 +0530

    cleaned up code 2

--- src/main.rs
+++ src/main.rs
@@ -51,12 +51,12 @@ pub struct ClientConfig {
 
 pub type SavingsVaultClient = Client<Rc<Keypair>>;
 
-pub fn setup_client(sugar_config: &ClientConfig) -> Result<SavingsVaultClient> {
-    let rpc_url = sugar_config.rpc_url.clone();
+pub fn setup_client(config: &ClientConfig) -> Result<SavingsVaultClient> {
+    let rpc_url = config.rpc_url.clone();
     let ws_url = rpc_url.replace("http", "ws");
     let cluster = Cluster::Custom(rpc_url, ws_url);
 
-    let key_bytes = sugar_config.keypair.to_bytes();
+    let key_bytes = config.keypair.to_bytes();
     let signer = Rc::new(Keypair::from_bytes(&key_bytes)?);
 
     let opts = CommitmentConfig::confirmed();
